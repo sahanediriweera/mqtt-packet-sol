a5a7f40fc4e58ee328dc1ead5d84739f2fa7eb10
sahanediriweera <rsahanediriweera@gmail.com>
2025-10-15 20:14:46 +0530

    add-header-creation

--- src/mqtt.cpp
+++ src/mqtt.cpp
@@ -200,3 +200,86 @@ int unpack_mqtt_packet(const unsigned char *buf,union mqtt_packet *pkt){
   }
   return rc;
 }
+
+union mqtt_header *mqtt_packet_header(unsigned char byte){
+  static union mqtt_header header;
+  header.byte = byte;
+  return &header;
+}
+
+struct mqtt_ack *mqtt_packet_ack(unsigned char byte,unsigned short pkt_id){
+  static struct mqtt_ack ack;
+  ack.header.byte = byte;
+  ack.pkt_id = pkt_id;
+  return &ack;
+}
+
+struct mqtt_ack *mqtt_packet_ack(unsigned char byte,unsigned char pkt_id){
+  static struct mqtt_ack ack;
+  ack.header.byte = byte;
+  ack.pkt_id = pkt_id;
+  return &ack;
+}
+
+struct mqtt_connack *mqtt_packet_conack(unsigned char byte, unsigned char cflags, unsigned char rc){
+  static struct mqtt_connack connack;
+  connack.header.byte = byte;
+  connack.byte = cflags;
+  connack.rc = rc;
+  return &connack;
+}
+
+struct mqtt_suback *mqtt_packet_suback(unsigned char byte,unsigned short pkt_id, unsigned char *rcs,unsigned short rcslen){
+  struct mqtt_suback *suback = (mqtt_suback*) malloc(sizeof(mqtt_suback));
+  suback->header.byte = byte;
+  suback->pkt_id = pkt_id;
+  suback->rcslen = rcslen;
+  suback->rcs = (unsigned char *) malloc(rcslen);
+  memcpy(suback->rcs,rcs, rcslen);
+  return suback;
+}
+
+struct mqtt_publish *mqtt_packet_publish(unsigned char byte,unsigned short pkt_id,size_t topiclen,unsigned char *topic,size_t payloadlen,unsigned char *payload){
+  struct mqtt_publish *publish = malloc(sizeof(*publish));
+  publish->header.byte = byte;
+  publish->pkt_id = pkt_id;
+  publish->topiclen = topiclen;
+  publish->topic = topic;
+  publish->payload = payload;
+  publish->payloadlen = payloadlen;
+  return publish;
+}
+
+void mqtt_packet_release(union mqtt_packet *pkt,unsigned type){
+  switch (type) {
+    case CONNECT:
+      free(pkt->connect.payload.client_id);
+      if(pkt->connect.bits.username == 1){
+          free(pkt->connect.payload.username);
+      }
+      if(pkt->connect.bits.password == 1){
+        free(pkt->connect.payload.password);
+      }
+      if(pkt->connect.bits.will == 1){
+        free(pkt->connect.payload.will_message);
+        free(pkt->connect.payload.will_topic);
+      }
+      break;
+    case SUBSCRIBE:
+    case UNSUBSCRIBE:
+      for(unsigned i =0 ;i < pkt->subscribe.tuples_len;i++)}{
+        free(pkt->subscribe.tuples[i].topic);
+      }
+      free(pkt->subscribe.tuples);
+      break;
+    case SUBACK:
+      free(pkt->suback.rcs);
+      break;
+    case PUBLISH:
+        free(pkt->publish.topic);
+        free(pkt->publish.payload);
+        break;
+    default:
+        break;
+  }
+}
--- src/mqtt.h
+++ src/mqtt.h
@@ -154,12 +154,6 @@ int unpack_mqtt_packet(const unsigned char*, union mqtt_packet*);
 unsigned char pack_mqtt_packet(const union mqtt_packet *,unsigned);
 
 union mqtt_header *mqtt_packet_header(unsigned char);
-struct mqtt_ack *mqtt_packet_ack(unsigned char,unsigned short);
-struct mqtt_connack *mqtt_packet_suback(unsigned char,unsigned short,unsigned char*,unsigned short);
-struct mqtt_publish *mqtt_packet_publish(unsigned char,unsigned short,size_t,unsigned char*,size_t,unsigned char*);
-void mqtt_packet_release(union mqtt_packet *,unsigned);
-
-union mqtt_header *mqtt_packet_header(unsigned char);
 struct mqtt_ack *mqtt_packet_ack(unsigned char, unsigned short);
 struct mqtt_connack *mqtt_connack(unsigned char,unsigned char,unsigned char);
 struct mqtt_suback *mqtt_packet_suback(unsigned char,unsigned char,unsigned char*,unsigned short);
