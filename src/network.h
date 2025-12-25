d858d89fcb055442f23f0e14cd5943a568167032
Sahan Ediriweera <sahanediriweera@MacBookAir.mshome.net>
2025-12-25 11:42:33 +0530

    add ev header functions

--- src/network.h
+++ src/network.h
@@ -24,4 +24,54 @@ ssize_t send_bytes(int, const unsigned char *, size_t);
 
 ssize_t recv_bytes(int,unsigned char *,size_t);
 
+struct evloop {
+    int epollfd;
+    int max_events;
+    int timeout;
+    int status;
+    struct epoll_event *events;
+
+    int periodic_maxsize;
+    int periodic_nr;
+
+    struct {
+        int timerfd;
+        struct closure *closure;
+    } ** periodic_tasks;
+};
+
+typedef void callback(struct evloop *, void *);
+
+struct closure {
+    int fd;
+    void *obj;
+    void *args;
+    char closure_id[UUID_LEN];
+    struct bytestring *payload;
+    callback *call;
+};
+
+struct evloop *evloop_create(int,int);
+
+void evloop_init(struct evloop *,int,int);
+void evloop_free(struct evloop *);
+
+int evloop_wait(struct evloop *);
+
+void evloop_add_callback(struct evloop *, struct closure *);
+
+void evloop_add_periodic_task(struct evloop *, int, unsigned long long, struct closure *);
+
+int evloop_del_callback(struct evloop *, struct closure *);
+
+int evloop_rearm_callback_read(struct evloop *, struct closure *);
+
+int evloop_rearm_callback_write(struct evloop *, struct closure *);
+
+int epoll_add(int,int,int,void *);
+
+int epoll_mod(int, int, int, void *);
+
+int epoll_del(int, int);
+
 #endif
